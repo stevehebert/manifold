60f3131312be65e32b7a40de05779cc49c5e6096
s_hebert <none@none>
2011-06-17 17:54:43 -0500

    separated named resolvers from unnamed resolvers

--- src/FunctionComposition/IPipeline.cs
+++ src/FunctionComposition/IPipeline.cs
@@ -1,61 +1,26 @@
 ﻿using System;
-using Wormhole.Pipeline;
 
 namespace Wormhole
 {
     public interface IPipeline<TNameType, TInput, TOutput >
     {
-        TOutput Execute(TInput input);
-
+        /// <summary>
+        /// Executes the specified named function.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="input">The input.</param>
+        /// <returns></returns>
         TOutput Execute(TNameType name, TInput input);
-    }
-
-    public class Pipeline<TNameType, TInput, TOutput> : IPipeline<TNameType, TInput,TOutput>
-    {
-        private readonly NamedResolver<TInput, TOutput> _namedResolver;
-
-        public Pipeline(NamedResolver<TInput, TOutput> namedResolver)
-        {
-            _namedResolver = namedResolver;
-        }
-        
-        public TOutput Execute(TInput input)
-        {
-            return _namedResolver.Execute(new DefaultPipeline<TInput, TOutput>(), input);
-        }
-
-        public TOutput Execute(TNameType name, TInput input)
-        {
-            return _namedResolver.Execute(name, input);
-        }
     }
 
 
     public interface IPipeline<TInput, TOutput>
     {
+        /// <summary>
+        /// Executes the specified function.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>The output.</returns>
         TOutput Execute(TInput input);
-
-        TOutput Execute(object name, TInput input);
-    }
-
-    public class Pipeline<TInput, TOutput> : IPipeline<TInput, TOutput> where TOutput : class
-    {
-        private readonly NamedResolver<TInput, TOutput> _namedResolver;
-        
-        public Pipeline(NamedResolver<TInput, TOutput> namedResolver)
-        {
-            _namedResolver = namedResolver;
-        }
-
-        public TOutput Execute(TInput input)
-        {
-            return _namedResolver.Execute(new DefaultPipeline<TInput,TOutput>(), input);
-        }
-
-        public TOutput Execute(object name, TInput input)
-        {
-            return _namedResolver.Execute(name, input);
-
-        }
     }
 }
---
+++ src/FunctionComposition/Pipeline/Pipeline.cs
@@ -0,0 +1,46 @@
+﻿namespace Wormhole.Pipeline
+{
+    /// <summary>
+    /// This resolver is used for default (or unnamed) pipelines
+    /// </summary>
+    /// <typeparam name="TInput">The type of the input.</typeparam>
+    /// <typeparam name="TOutput">The type of the output.</typeparam>
+    public class Pipeline<TInput, TOutput> : IPipeline<TInput, TOutput> where TOutput : class
+    {
+        private readonly NamedResolver<TInput, TOutput> _namedResolver;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Pipeline&lt;TInput, TOutput&gt;"/> class.
+        /// </summary>
+        /// <param name="namedResolver">The named resolver.</param>
+        public Pipeline(NamedResolver<TInput, TOutput> namedResolver)
+        {
+            _namedResolver = namedResolver;
+        }
+
+        /// <summary>
+        /// Executes the specified input.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>returns the desired output</returns>
+        public TOutput Execute(TInput input)
+        {
+            return _namedResolver.Execute(new DefaultPipeline<TInput,TOutput>(), input);
+        }
+    }
+
+    public class Pipeline<TNameType, TInput, TOutput> : IPipeline<TNameType, TInput,TOutput>
+    {
+        private readonly NamedResolver<TInput, TOutput> _namedResolver;
+
+        public Pipeline(NamedResolver<TInput, TOutput> namedResolver)
+        {
+            _namedResolver = namedResolver;
+        }
+
+        public TOutput Execute(TNameType name, TInput input)
+        {
+            return _namedResolver.Execute(name, input);
+        }
+    }
+}
