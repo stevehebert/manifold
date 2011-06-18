8f5b75ec0d2a4d42981ed99d01239cc045116190
s_hebert <none@none>
2011-06-18 07:20:49 -0500

    adding exception tests

---
+++ src/FunctionComposition.Tests/Wormhole/Pipeline/MismatchedClosingTypeDeclarationExceptionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Wormhole.Pipeline;
+
+namespace Wormhole.Tests.Wormhole.Pipeline
+{
+    [TestFixture]
+    public class MismatchedClosingTypeDeclarationExceptionTests
+    {
+        [Test]
+        public void verify_exception_consistency()
+        {
+            Assert.That(new MismatchedClosingTypeDeclarationException("hello").Message, Is.EqualTo("hello"));
+            Assert.That(new MismatchedClosingTypeDeclarationException("hello", new InvalidOperationException()).InnerException, Is.TypeOf<InvalidOperationException>());
+        }
+    }
+}
