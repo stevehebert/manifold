29843551b52e88358d0d94fa72e142365962a70b
s_hebert <none@none>
2011-06-19 23:45:37 -0500

    updated rake

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("FunctionComposition")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
