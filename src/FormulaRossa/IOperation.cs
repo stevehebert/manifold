c8ce31ed6ecaccfec7168c76d0e3c5230c36f5c2
Steve Hebert <steve.hebert@gmail.com>
2011-10-06 16:54:06 -0500

    broke out another class-file relationship

--- src/FormulaRossa/IOperation.cs
+++ src/FormulaRossa/IOperation.cs
@@ -8,21 +8,4 @@ namespace FormulaRossa
     {
         Func<Injector, object, object> GetClosure();
     }
-
-    public class InjectedOperation<TType, TInput, TOutput> : IOperation where TType : IPipelineTask<TInput, TOutput>
-    {
-        public Func<Injector, object, object> GetClosure()
-        {
-            return (injector, o) =>
-                       {
-                           var item = (TType) injector(typeof (TType));
-
-                           if (item == null) throw new InvalidOperationException();
-
-                           var incoming = (TInput) o;
-
-                           return item.Execute(incoming);
-                       };
-        }
-    }
 }
