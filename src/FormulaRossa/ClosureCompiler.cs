a5bed66aeac3d66bce0e7c5773d0f98584effccd
Steve Hebert <steve.hebert@gmail.com>
2011-10-06 17:19:04 -0500

    added closure compiler with basic function nesting

---
+++ src/FormulaRossa/ClosureCompiler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FormulaRossa
+{
+    public class ClosureCompiler
+    {
+        public Func<Injector, object, object> Compile(Stack<IOperation> operations)
+        {
+            Func<Injector, object, object> functionDefinition = operations.Pop().GetClosure();
+            while( operations.Count > 0)
+            {
+                var localFn = functionDefinition;
+                var fn = operations.Pop().GetClosure();
+
+                functionDefinition = (a, b) => fn(a, localFn(a, b));
+            }
+
+            return functionDefinition;
+        }
+    }
+}
