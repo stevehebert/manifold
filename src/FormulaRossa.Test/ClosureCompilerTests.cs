2b3bac862aa13675ff6146618660ceb69b975ab2
Steve Hebert <steve.hebert@gmail.com>
2011-10-07 10:02:32 -0500

    added ordered testing of the closure compiler

---
+++ src/FormulaRossa.Test/ClosureCompilerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace FormulaRossa.Test
+{
+    [TestFixture]
+    public class ClosureCompilerTests
+    {
+        private class Oper : IOperation
+        {
+            private readonly Func<Injector, object, object> _func;
+            public Oper(Func<Injector, object, object> func )
+            {
+                _func = func;
+            }
+            public Func<Injector, object, object> GetClosure()
+            {
+                return _func;
+            }
+        }
+
+        [Test]
+        public void execute_ordered_test()
+        {
+            var item = new ClosureCompiler();
+
+            var oper = new Oper((a, o) => (int) o*2);
+
+            var f = item.Compile(new Queue<IOperation>(new[] {oper}));
+
+            Assert.That(f(null, 5), Is.EqualTo(10));
+        }
+
+        [Test]
+        public void execute_ordered_test2()
+        {
+            var item = new ClosureCompiler();
+
+            var f = item.Compile(new Queue<IOperation>(new[] { new Oper((a, o) => (int)o * 4), new Oper((a, o) => (int)o - 2) }));
+
+            Assert.That(f(null, 5), Is.EqualTo(18));
+        }
+
+        [Test]
+        public void execute_ordered_test3()
+        {
+            var item = new ClosureCompiler();
+
+            var f = item.Compile(new Queue<IOperation>(new[] { new Oper((a, o) => (int)o * 4), new Oper((a, o) => (int)o - 2), new Oper((a, o) => (int)o /3 ) }));
+
+            Assert.That(f(null, 5), Is.EqualTo(6));
+        }
+    }
+}
--- src/FormulaRossa/ClosureCompiler.cs
+++ src/FormulaRossa/ClosureCompiler.cs
@@ -5,13 +5,18 @@ namespace FormulaRossa
 {
     public class ClosureCompiler
     {
-        public Func<Injector, object, object> Compile(Stack<IOperation> operations)
+        public Func<Injector, object, object> Compile(IEnumerable<IOperation> operations )
         {
-            Func<Injector, object, object> functionDefinition = operations.Pop().GetClosure();
+            return Compile(new Queue<IOperation>(operations));
+        }
+        
+        public Func<Injector, object, object> Compile(Queue<IOperation> operations)
+        {
+            Func<Injector, object, object> functionDefinition = operations.Dequeue().GetClosure();
             while( operations.Count > 0)
             {
                 var localFn = functionDefinition;
-                var fn = operations.Pop().GetClosure();
+                var fn = operations.Dequeue().GetClosure();
 
                 functionDefinition = (a, b) => fn(a, localFn(a, b));
             }
